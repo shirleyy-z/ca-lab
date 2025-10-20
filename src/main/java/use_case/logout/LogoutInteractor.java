64b31f277f2fd52f974bfbd446e8f164ad67dd19
iaincampbell <iainmcampbell95@gmail.com>
2025-10-20 17:56:56 -0400

    LogoutInteractor

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -4,8 +4,8 @@ package use_case.logout;
  * The Logout Interactor.
  */
 public class LogoutInteractor implements LogoutInputBoundary {
-    private final LogoutUserDataAccessInterface userDataAccessObject;
-    private final LogoutOutputBoundary logoutPresenter;
+    private LogoutUserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
